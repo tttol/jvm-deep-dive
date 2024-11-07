2430aa79869cd350313a305c0214f1b7ea7ef341
t-takahashi <tttol.t4k.b1z@gmail.com>
2024-11-08 07:08:13 +0900

    rm comment

--- CrashDemo.c
+++ CrashDemo.c
@@ -1,8 +1,8 @@
 #include <jni.h>
-#include "CrashDemo.h" // 生成されたヘッダファイルをインクルード
+#include "CrashDemo.h" 
 #include <stdlib.h>
 
 JNIEXPORT void JNICALL Java_CrashDemo_causeSegmentationFault(JNIEnv *env, jobject obj) {
-    int *ptr = NULL; // NULLポインタ
-    *ptr = 1;        // NULL参照でセグメンテーション違反を発生させる
+    int *ptr = NULL; 
+    *ptr = 1;        
 }
--- CrashDemo.java
+++ CrashDemo.java
@@ -1,13 +1,11 @@
 public class CrashDemo {
-    // ネイティブライブラリをロード
     static {
-        System.loadLibrary("crash"); // ライブラリ名（crash.soやcrash.dllを指します）
+        System.loadLibrary("crash");
     }
 
-    // ネイティブメソッドの宣言
     private native void causeSegmentationFault();
 
     public static void main(String[] args) {
-        new CrashDemo().causeSegmentationFault(); // メソッド呼び出し
+        new CrashDemo().causeSegmentationFault();
     }
 }
